2e53ca96ad52aeb30b67388794ea4c0c8c7042fb
Theza Sri Madesh <thezasti1997@gmail.com>
2020-09-18 21:00:47 +0530

    Initial Commit

---
+++ src/test/java/sample1/LoginTest.java
@@ -0,0 +1,32 @@
+package sample1;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+
+public class LoginTest {
+	public static WebDriver driver;
+	@BeforeSuite
+	public void setUp()
+	{
+		System.setProperty("webdriver.gecko.driver", "C:\\jars\\geckodriver.exe");
+		driver=new FirefoxDriver();
+		
+	}
+	@Test
+public void doLogin()
+{
+		
+	driver.get("http://gmail.com");
+	//driver.findElement(By.id("identifierID")).sendKeys("thezasri1997@gmail.com");
+	//driver.findElement(By.id("Password")).sendKeys("sdfs");
+	}
+	@AfterSuite
+	public void tearDown()
+	{
+		driver.quit();
+	}
+}
